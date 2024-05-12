d97913d27f2b8219cc72411f711a17910db57ba7
Osura De Silva <osuradesilva@hotmail.com>
2024-05-12 16:00:07 +0530

    Trying livewire and volt

---
+++ resources/views/livewire/HelloWorld.blade.php
@@ -0,0 +1,33 @@
+<?php
+
+use function Livewire\Volt\{state, mount};
+
+state([
+    'count' => 0,
+    'name' => null,
+]);
+mount(function () {
+    //$this->count = 1;
+});
+
+$decrement = function (){
+    $this->count--;
+};
+
+
+$increment = function (){
+    $this->count++;
+};
+
+?>
+
+<div>
+    <div class="flex space-x-4">
+        <button wire:click="decrement">-</button>
+        <span>{{$count}}</span>
+        <button wire:click="increment">+</button>
+    </div>
+
+    <input type="text" wire:model.live="name">
+    hello {{$name}}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return view('hello', [
+        'name' => 'Laravel',
+        'greeting' => 'Hello, Laravel!'
+    ]);
+});
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
