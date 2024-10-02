aaec2e6ff563f105b652852aaead03f3fb910607
Osura De Silva <osuradesilva@hotmail.com>
2024-10-02 17:35:48 +0530

    adding sponsor partner details

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -6,22 +6,22 @@
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <!-- Partner 1 -->
-            <a href="https://partner1-link.com" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
+            <a href="https://www.amd.com/en.html" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
                 <img src="{{ asset('images/partner1.jpg') }}" alt="Partner 1" class="object-contain max-h-24 transition-transform duration-300 transform group-hover:scale-110">
             </a>
 
             <!-- Partner 2 -->
-            <a href="https://partner2-link.com" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
+            <a href="https://rog.asus.com/" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
                 <img src="{{ asset('images/partner2.jpg') }}" alt="Partner 2" class="object-contain max-h-24 transition-transform duration-300 transform group-hover:scale-110">
             </a>
 
             <!-- Partner 3 -->
-            <a href="https://partner3-link.com" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
+            <a href="https://www.spectrum.net/" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
                 <img src="{{ asset('images/partner3.jpg') }}" alt="Partner 3" class="object-contain max-h-24 transition-transform duration-300 transform group-hover:scale-110">
             </a>
 
             <!-- Partner 4 -->
-            <a href="https://partner4-link.com" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
+            <a href="https://www.samsung.com/us/smartphones/" target="_blank" class="relative group bg-gray-800 rounded-lg flex items-center justify-center h-48 hover:bg-gray-700 transition duration-300 ease-in-out">
                 <img src="{{ asset('images/partner4.jpg') }}" alt="Partner 4" class="object-contain max-h-24 transition-transform duration-300 transform group-hover:scale-110">
             </a>
         </div>
