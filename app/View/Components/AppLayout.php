df17dc1a083af2274c713d31531e6bf081b741e6
Osura Hansaja <osuradesilva@hotmail.com>
2024-10-23 20:55:32 +0530

    Made changes to frontend layout component

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -8,7 +8,7 @@ use Illuminate\View\View;
 class AppLayout extends Component
 {
     /**
-     * Get the view / contents that represents the component.
+     * Get the view / contents that represents the component. Update the layout.
      */
     public function render(): View
     {
